6953e4b07b3d2ac0194cc888f49e09fd641afa30
ezrateshale <ezra.teshale12@gmail.com>
2026-04-10 15:31:59 -0500

    feat:Payroll refractoring

--- src/main/java/com/pluralsight/Payroll.java
+++ src/main/java/com/pluralsight/Payroll.java
@@ -10,13 +10,17 @@ public class Payroll {
         System.out.print("What is your pay rate? ");
         float payRate = input.nextFloat();
 
+        extracted(hoursWorked, payRate, name);
+    }
+
+    private static void extracted(float hoursWorked, float payRate, String name) {
         if(hoursWorked > 40){
            float totalPay = ((hoursWorked - 40)*(payRate * 1.5f)) + (40 * payRate);
-           System.out.printf("%s, your gross pay with overtime is: %.2f" ,name,totalPay);
+           System.out.printf("%s, your gross pay with overtime is: %.2f" , name,totalPay);
         }
         else{
            float totalPay = hoursWorked * payRate;
-           System.out.printf("%s, your gross pay without overtime is: %.2f" ,name,totalPay);
+           System.out.printf("%s, your gross pay without overtime is: %.2f" , name,totalPay);
         }
     }
 }
