4bf0136c89b478884b1f9b0fd8be4436b665f922
ezrateshale <ezra.teshale12@gmail.com>
2026-04-10 10:15:45 -0500

    exercise 4: Edits

--- src/main/java/com/pluralsight/Payroll.java
+++ src/main/java/com/pluralsight/Payroll.java
@@ -2,20 +2,20 @@ package com.pluralsight;
 import java.util.Scanner;
 public class Payroll {
     public static void main(String[] args) {
-       Scanner input=new Scanner(System.in);
-        System.out.println("What is your name");
-        String name=input.nextLine();
-        System.out.println("How many hours have you worked?");
-        float hoursWorked=input.nextFloat();
-        System.out.println("What is your pay rate?");
-        float payRate=input.nextFloat();
+       Scanner input = new Scanner(System.in);
+        System.out.print("What is your name");
+        String name = input.nextLine();
+        System.out.print("How many hours have you worked?");
+        float hoursWorked = input.nextFloat();
+        System.out.print("What is your pay rate?");
+        float payRate = input.nextFloat();
 
-        if(hoursWorked>40){
-           float totalPay= ((float)((hoursWorked-40)*(payRate*1.5))+((40*payRate)));
+        if(hoursWorked > 40){
+           float totalPay = ((hoursWorked-40)*(payRate*1.5f)) + ((40*payRate));
             System.out.printf("%s, your gross pay with overtime is: %.2f",name,totalPay);
         }
         else{
-           float totalPay=(((hoursWorked)*(payRate)));
+           float totalPay = hoursWorked * payRate;
             System.out.printf("%s, your gross pay without overtime is: %.2f",name,totalPay);
         }
     }
